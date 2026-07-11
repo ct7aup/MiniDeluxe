a5ee536dd7c0c555eee46cfaadfea77f3e6f5c4d
ct7aup <ct7aup@gmail.com>
2026-07-11 10:19:41 +0100

    Add new HRD protocol listener

--- MiniDeluxe/MiniDeluxeForm.Designer.cs
+++ MiniDeluxe/MiniDeluxeForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MiniDeluxe
+namespace MiniDeluxe
 {
     partial class MiniDeluxeForm
     {
@@ -51,76 +51,87 @@
             this.label6 = new System.Windows.Forms.Label();
             this.txtRIOXIP = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tbNewProtocolRadioName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbNewProtocolContextId = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbNewProtocolPort = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbEnableNewProtocol = new System.Windows.Forms.CheckBox();
+            this.cbEnableLegacyProtocol = new System.Windows.Forms.CheckBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(172, 189);
+            //
+            this.btnSave.Location = new System.Drawing.Point(172, 218);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(64, 23);
             this.btnSave.TabIndex = 4;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
+            //
             // lblStatus
-            // 
+            //
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(3, 194);
+            this.lblStatus.Location = new System.Drawing.Point(3, 223);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(47, 13);
             this.lblStatus.TabIndex = 6;
             this.lblStatus.Text = "lblStatus";
-            // 
+            //
             // cbLocalOnly
-            // 
+            //
             this.cbLocalOnly.AutoSize = true;
             this.cbLocalOnly.Checked = true;
             this.cbLocalOnly.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbLocalOnly.Location = new System.Drawing.Point(6, 158);
+            this.cbLocalOnly.Location = new System.Drawing.Point(6, 187);
             this.cbLocalOnly.Name = "cbLocalOnly";
             this.cbLocalOnly.Size = new System.Drawing.Size(135, 17);
             this.cbLocalOnly.TabIndex = 13;
             this.cbLocalOnly.Text = "Local connections only";
             this.cbLocalOnly.UseVisualStyleBackColor = true;
-            // 
+            //
             // btnAbout
-            // 
-            this.btnAbout.Location = new System.Drawing.Point(123, 189);
+            //
+            this.btnAbout.Location = new System.Drawing.Point(123, 218);
             this.btnAbout.Name = "btnAbout";
             this.btnAbout.Size = new System.Drawing.Size(44, 23);
             this.btnAbout.TabIndex = 14;
             this.btnAbout.Text = "About";
             this.btnAbout.UseVisualStyleBackColor = true;
             this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
-            // 
+            //
             // btnCheckForUpdate
-            // 
+            //
             this.btnCheckForUpdate.Enabled = false;
-            this.btnCheckForUpdate.Location = new System.Drawing.Point(123, 218);
+            this.btnCheckForUpdate.Location = new System.Drawing.Point(123, 247);
             this.btnCheckForUpdate.Name = "btnCheckForUpdate";
             this.btnCheckForUpdate.Size = new System.Drawing.Size(113, 23);
             this.btnCheckForUpdate.TabIndex = 15;
             this.btnCheckForUpdate.Text = "Check for Update";
             this.btnCheckForUpdate.UseVisualStyleBackColor = true;
             this.btnCheckForUpdate.Click += new System.EventHandler(this.btnCheckForUpdate_Click);
-            // 
+            //
             // tabControl1
-            // 
+            //
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(6, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(241, 151);
+            this.tabControl1.Size = new System.Drawing.Size(241, 180);
             this.tabControl1.TabIndex = 11;
-            // 
+            //
             // tabPage1
-            // 
+            //
             this.tabPage1.Controls.Add(this.groupBox1);
             this.tabPage1.Controls.Add(this.tbPort);
             this.tabPage1.Controls.Add(this.label2);
@@ -133,9 +144,9 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "PowerSDR";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
+            //
             // groupBox1
-            // 
+            //
             this.groupBox1.Controls.Add(this.tbLow);
             this.groupBox1.Controls.Add(this.tbHigh);
             this.groupBox1.Controls.Add(this.label3);
@@ -146,69 +157,69 @@
             this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Update Intervals (msec)";
-            // 
+            //
             // tbLow
-            // 
+            //
             this.tbLow.Location = new System.Drawing.Point(100, 38);
             this.tbLow.Name = "tbLow";
             this.tbLow.Size = new System.Drawing.Size(51, 20);
             this.tbLow.TabIndex = 10;
             this.tbLow.Text = "5000";
-            // 
+            //
             // tbHigh
-            // 
+            //
             this.tbHigh.Location = new System.Drawing.Point(100, 15);
             this.tbHigh.Name = "tbHigh";
             this.tbHigh.Size = new System.Drawing.Size(51, 20);
             this.tbHigh.TabIndex = 9;
             this.tbHigh.Text = "1000";
-            // 
+            //
             // label3
-            // 
+            //
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(28, 18);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "High Priority:";
-            // 
+            //
             // label4
-            // 
+            //
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(30, 41);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Low Priority:";
-            // 
+            //
             // tbPort
-            // 
+            //
             this.tbPort.Location = new System.Drawing.Point(117, 30);
             this.tbPort.Name = "tbPort";
             this.tbPort.Size = new System.Drawing.Size(100, 20);
             this.tbPort.TabIndex = 15;
             this.tbPort.Text = "7809";
-            // 
+            //
             // label2
-            // 
+            //
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(21, 30);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "HRD Server Port:";
-            // 
+            //
             // label1
-            // 
+            //
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(3, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 13);
             this.label1.TabIndex = 13;
             this.label1.Text = "PowerSDR Serial Port:";
-            // 
+            //
             // cbSerialport
-            // 
+            //
             this.cbSerialport.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbSerialport.FormattingEnabled = true;
             this.cbSerialport.Items.AddRange(new object[] {
@@ -217,9 +228,9 @@
             this.cbSerialport.Name = "cbSerialport";
             this.cbSerialport.Size = new System.Drawing.Size(100, 21);
             this.cbSerialport.TabIndex = 12;
-            // 
+            //
             // tabPage2
-            // 
+            //
             this.tabPage2.Controls.Add(this.btnRIOXTest);
             this.tabPage2.Controls.Add(this.txtRIOXport);
             this.tabPage2.Controls.Add(this.label6);
@@ -232,9 +243,9 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "RIOX/DDUtil";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
+            //
             // btnRIOXTest
-            // 
+            //
             this.btnRIOXTest.Location = new System.Drawing.Point(122, 69);
             this.btnRIOXTest.Name = "btnRIOXTest";
             this.btnRIOXTest.Size = new System.Drawing.Size(75, 23);
@@ -242,46 +253,139 @@
             this.btnRIOXTest.Text = "Test";
             this.btnRIOXTest.UseVisualStyleBackColor = true;
             this.btnRIOXTest.Click += new System.EventHandler(this.btnRIOXTest_Click);
-            // 
+            //
             // txtRIOXport
-            // 
+            //
             this.txtRIOXport.Location = new System.Drawing.Point(97, 34);
             this.txtRIOXport.Name = "txtRIOXport";
             this.txtRIOXport.Size = new System.Drawing.Size(100, 20);
             this.txtRIOXport.TabIndex = 3;
             this.txtRIOXport.Text = "1234";
-            // 
+            //
             // label6
-            // 
+            //
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(60, 36);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(29, 13);
             this.label6.TabIndex = 2;
             this.label6.Text = "Port:";
-            // 
+            //
             // txtRIOXIP
-            // 
+            //
             this.txtRIOXIP.Location = new System.Drawing.Point(97, 7);
             this.txtRIOXIP.Name = "txtRIOXIP";
             this.txtRIOXIP.Size = new System.Drawing.Size(100, 20);
             this.txtRIOXIP.TabIndex = 1;
             this.txtRIOXIP.Text = "localhost";
-            // 
+            //
             // label5
-            // 
+            //
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(6, 10);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(83, 13);
             this.label5.TabIndex = 0;
             this.label5.Text = "RIOX Server IP:";
-            // 
+            //
+            // tabPage3
+            //
+            this.tabPage3.Controls.Add(this.tbNewProtocolRadioName);
+            this.tabPage3.Controls.Add(this.label9);
+            this.tabPage3.Controls.Add(this.tbNewProtocolContextId);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.tbNewProtocolPort);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.cbEnableNewProtocol);
+            this.tabPage3.Controls.Add(this.cbEnableLegacyProtocol);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(233, 154);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "HRD Protocols";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            //
+            // tbNewProtocolRadioName
+            //
+            this.tbNewProtocolRadioName.Location = new System.Drawing.Point(88, 105);
+            this.tbNewProtocolRadioName.Name = "tbNewProtocolRadioName";
+            this.tbNewProtocolRadioName.Size = new System.Drawing.Size(128, 20);
+            this.tbNewProtocolRadioName.TabIndex = 7;
+            this.tbNewProtocolRadioName.Text = "SMART SDR";
+            //
+            // label9
+            //
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Radio name:";
+            //
+            // tbNewProtocolContextId
+            //
+            this.tbNewProtocolContextId.Location = new System.Drawing.Point(88, 79);
+            this.tbNewProtocolContextId.Name = "tbNewProtocolContextId";
+            this.tbNewProtocolContextId.Size = new System.Drawing.Size(54, 20);
+            this.tbNewProtocolContextId.TabIndex = 5;
+            this.tbNewProtocolContextId.Text = "179";
+            //
+            // label8
+            //
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Context Id:";
+            //
+            // tbNewProtocolPort
+            //
+            this.tbNewProtocolPort.Location = new System.Drawing.Point(88, 53);
+            this.tbNewProtocolPort.Name = "tbNewProtocolPort";
+            this.tbNewProtocolPort.Size = new System.Drawing.Size(54, 20);
+            this.tbNewProtocolPort.TabIndex = 3;
+            this.tbNewProtocolPort.Text = "7810";
+            //
+            // label7
+            //
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(56, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Port:";
+            //
+            // cbEnableNewProtocol
+            //
+            this.cbEnableNewProtocol.AutoSize = true;
+            this.cbEnableNewProtocol.Checked = true;
+            this.cbEnableNewProtocol.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbEnableNewProtocol.Location = new System.Drawing.Point(6, 30);
+            this.cbEnableNewProtocol.Name = "cbEnableNewProtocol";
+            this.cbEnableNewProtocol.Size = new System.Drawing.Size(154, 17);
+            this.cbEnableNewProtocol.TabIndex = 1;
+            this.cbEnableNewProtocol.Text = "Enable new HRD protocol";
+            this.cbEnableNewProtocol.UseVisualStyleBackColor = true;
+            //
+            // cbEnableLegacyProtocol
+            //
+            this.cbEnableLegacyProtocol.AutoSize = true;
+            this.cbEnableLegacyProtocol.Checked = true;
+            this.cbEnableLegacyProtocol.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbEnableLegacyProtocol.Location = new System.Drawing.Point(6, 7);
+            this.cbEnableLegacyProtocol.Name = "cbEnableLegacyProtocol";
+            this.cbEnableLegacyProtocol.Size = new System.Drawing.Size(166, 17);
+            this.cbEnableLegacyProtocol.TabIndex = 0;
+            this.cbEnableLegacyProtocol.Text = "Enable legacy HRD protocol";
+            this.cbEnableLegacyProtocol.UseVisualStyleBackColor = true;
+            //
             // MiniDeluxeForm
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(248, 247);
+            this.ClientSize = new System.Drawing.Size(248, 276);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.btnCheckForUpdate);
             this.Controls.Add(this.btnAbout);
@@ -302,6 +406,8 @@
             this.groupBox1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -331,6 +437,14 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtRIOXIP;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox tbNewProtocolRadioName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbNewProtocolContextId;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbNewProtocolPort;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox cbEnableNewProtocol;
+        private System.Windows.Forms.CheckBox cbEnableLegacyProtocol;
     }
 }
-
